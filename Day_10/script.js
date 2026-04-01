a1ad0b96750c81ebe95034bf27d9ebda69620d35
Ashish Kushwaha <ashishbtech.cs@gmail.com>
2026-04-01 18:51:08 +0530

    day 10 completed

---
+++ Day_10/script.js
@@ -0,0 +1,23 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+let lastChecked = null
+
+function handleCheck(e) {
+  let inBetween = false
+
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(box => {
+      if (box === this || box === lastChecked) {
+        inBetween = !inBetween
+      }
+
+      if (inBetween) {
+        box.checked = true
+      }
+    })
+  }
+
+  lastChecked = this
+}
+
+checkboxes.forEach(box => box.addEventListener('click', handleCheck))
